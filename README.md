ca3f0374e99c7aa1fb483b50a43cd25abf6cce94
Yasunori Toshimitsu <yasu313nori@gmail.com>
2020-12-05 23:21:02 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# how to use
+connect LED strip to power, connect IR LED to pin 3.
+
+# How each command ID was found; brute-force search!
+https://www.instructables.com/BruteForce-Codefinding-for-Infrared-RGB-LEDstrip/
---
+++ artificial-sun.ino
@@ -0,0 +1,134 @@
+#include <IRremote.h>
+
+IRsend irsend;
+
+unsigned long command = 0;
+byte al, ar, cl, cr; // address & command byte and their inverse
+unsigned int a, c; // the complete address and command byte
+unsigned long total = 0L; // the 32 byte long to send to plug into the NEC protocol
+
+unsigned long second = 1000;
+unsigned long minute = second*60;
+
+namespace table{
+  byte on = 192;
+  byte off= 64;
+
+  // 16 stages
+  byte brighter = 0;
+  byte darker = 128;
+  
+  byte flash = 208;
+  byte smooth = 232;
+  byte fade = 240;
+  byte strobe = 200;
+  
+  byte W = 224;
+  
+  byte R = 32;
+  byte R_1 = 16;
+  byte R_2 = 48;
+  byte R_3 = 8;
+  byte R_4 = 40;
+  
+  
+  byte B = 96;
+  byte B_1 = 80;
+  byte B_2 = 112;
+  byte B_3 = 72;
+  byte B_4 = 104;
+  
+  
+  byte G = 160;
+  byte G_1 = 144;
+  byte G_2 = 177;
+  byte G_3 = 136;
+  byte G_4 = 168;
+}
+
+void setup() {
+  controlLED(table::on);
+  controlLED(table::W);
+  for (int i=0; i<4; i++){
+    delay(second * 10);
+    controlLED(table::darker);
+  }
+  controlLED(table::B_3);
+  for (int i=0; i<4; i++){
+    delay(second * 10);
+    controlLED(table::darker);
+  }
+  controlLED(table::R_4);
+  for (int i=0; i<4; i++){
+    delay(second * 10);
+    controlLED(table::darker);
+  }
+  controlLED(table::R_2);
+  for (int i=0; i<4; i++){
+    delay(second * 10);
+    controlLED(table::darker);
+  }
+  delay(second * 40);
+  controlLED(table::R_1);
+  delay(second * 40);
+  controlLED(table::R);
+  delay(second * 40);
+  controlLED(table::off);
+
+  delay(minute * (60 * 7 + 30));
+
+  controlLED(table::on);
+  controlLED(table::R);
+  delay(minute * 2);
+  controlLED(table::R_1);
+  delay(minute * 2);
+  controlLED(table::R_2);
+  for (int i=0; i<4; i++){
+    delay(second * 30);
+    controlLED(table::brighter);
+  }
+  controlLED(table::R_4);
+  for (int i=0; i<4; i++){
+    delay(second * 30);
+    controlLED(table::brighter);
+  }
+  controlLED(table::B_3);
+  for (int i=0; i<4; i++){
+    delay(second * 30);
+    controlLED(table::brighter);
+  }
+  controlLED(table::W);
+  for (int i=0; i<4; i++){
+    delay(second * 30);
+    controlLED(table::brighter);
+  }
+}
+
+void loop() {
+
+}
+
+void controlLED (byte command){
+  cl = command;
+  doen(al, cl);
+  total = combineint(c, a);
+  irsend.sendNEC(total, 32);
+}
+
+unsigned long combineint (unsigned int rechts, unsigned long links)
+{
+  /** makes a long from two integers*/
+  unsigned long value2 = 0L;
+  value2 = links << 16;
+  value2 = value2 | (rechts);
+  return value2;
+}
+
+unsigned int doen(byte ag, byte cg)
+{
+  /** invert address & command byte, and combine each into variables a & c */
+  ar = ag ^ 0xFF;
+  cr = cg ^ 0xFF;
+  a = 256 * ag + ar;
+  c = cg << 8 | cr;
+}
