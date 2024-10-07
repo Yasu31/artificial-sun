574d621825421758c2e96ca790c1a7dcb3cc78a9
Yasunori Toshimitsu <yasu313nori@gmail.com>
2024-10-07 23:54:46 +0200

    refactor to simplify

--- artificial-sun.ino
+++ artificial-sun.ino
@@ -1,68 +1,94 @@
 #include "SevSeg.h"
 SevSeg sevseg; // Instantiate a seven-segment object
-// https://github.com/DeanIsMe/SevSeg
 
-// Variables for the timer
+// Timer variables
 unsigned long lastInteractionTime = 0;
 bool displayOn = true;
 unsigned long setDuration = 7UL * 60 * 60 * 1000; // User-set duration
-unsigned long remainingTime = setDuration; // Remaining time in milliseconds
+unsigned long remainingTime = setDuration;        // Remaining time in milliseconds
 bool timerPaused = true;
 unsigned long lastBlinkTime = 0;
 bool displayBlinkState = true;
 unsigned long lastTimerUpdateTime = 0;
 
-// Variables for center button
-unsigned long centerButtonPressTime = 0;
-bool centerButtonLongPressHandled = false;
+// Center button long press duration
 const unsigned long LONG_PRESS_DURATION = 1000; // 1 second for long press
 
-// Variables for left button
-unsigned long lastIncrementTime_l = 0;
-unsigned long leftButtonPressStartTime = 0;
+// LED brightness variables
+float currentBrightness = 0.0;
+const float smoothingFactor = 0.002;
 
-// Variables for right button
-unsigned long lastIncrementTime_r = 0;
-unsigned long rightButtonPressStartTime = 0;
+// LED auto-off variables
+unsigned long ledTurnOffTime = 0;
+const unsigned long LED_AUTO_OFF_DURATION = 60UL * 60 * 1000;
 
-// Debounce variables
-bool buttonState_l = HIGH;
-bool lastButtonState_l = HIGH;
-unsigned long lastDebounceTime_l = 0;
-bool lastButtonPushed_l = false;
+// Constants for acceleration effect
+const unsigned long BASE_INTERVAL = 300; // Initial interval between increments
+const unsigned long MIN_INTERVAL = 10;   // Minimum interval between increments
+const unsigned long MAX_PRESS_DURATION = 6000;
 
-bool buttonState_c = HIGH;
-bool lastButtonState_c = HIGH;
-unsigned long lastDebounceTime_c = 0;
-bool lastButtonPushed_c = false;
-
-bool buttonState_r = HIGH;
-bool lastButtonState_r = HIGH;
-unsigned long lastDebounceTime_r = 0;
-bool lastButtonPushed_r = false;
-
+// Button pins
 const int buttonPin_l = 19;
 const int buttonPin_c = 20;
 const int buttonPin_r = 21;
 const int ledPin = 6; // High-power LED pin
 
-// Variables for LED brightness smoothing
-float currentBrightness = 0.0;
-const float smoothingFactor = 0.002;
+// Current time in milliseconds
+unsigned long currentMillis = 0;
 
-// Variable for LED auto-off
-unsigned long ledTurnOffTime = 0;
-const unsigned long LED_AUTO_OFF_DURATION = 60UL * 60 * 1000;
+// Button class to handle debouncing and state
+class Button {
+public:
+  int pin;
+  bool state;
+  bool lastState;
+  unsigned long lastDebounceTime;
+  bool lastButtonPushed;
+  unsigned long pressStartTime;
+  bool ignoreCurrentPress;
+  unsigned long lastIncrementTime;
 
-// Variables for button press handling
-bool ignoreCurrentPress_l = false;
-bool ignoreCurrentPress_r = false;
-bool ignoreCurrentPress_c = false;
+  Button(int pin) : pin(pin), state(HIGH), lastState(HIGH), lastDebounceTime(0),
+                    lastButtonPushed(false), pressStartTime(0),
+                    ignoreCurrentPress(false), lastIncrementTime(0) {}
 
-// Constants for acceleration effect
-const unsigned long BASE_INTERVAL = 300; // initial interval between increments
-const unsigned long MIN_INTERVAL = 10;   // minimum interval between increments
-const unsigned long MAX_PRESS_DURATION = 6000;
+  bool checkButton(unsigned long debounceDelay = 50) {
+    int reading = digitalRead(pin);
+
+    if (reading != lastState) {
+      lastDebounceTime = currentMillis;
+    }
+
+    if ((currentMillis - lastDebounceTime) > debounceDelay) {
+      if (reading != state) {
+        state = reading;
+        if (state == LOW) {
+          return true; // Button down event
+        }
+      }
+    }
+
+    lastState = reading;
+    return false;
+  }
+
+  bool isPressed() {
+    return state == LOW;
+  }
+
+  bool isReleased() {
+    return (!isPressed() && lastButtonPushed);
+  }
+
+  void updateLastButtonPushed() {
+    lastButtonPushed = isPressed();
+  }
+};
+
+// Instantiate button objects
+Button leftButton(buttonPin_l);
+Button centerButton(buttonPin_c);
+Button rightButton(buttonPin_r);
 
 void setup() {
   byte numDigits = 4;
@@ -73,159 +99,90 @@ void setup() {
   bool updateWithDelays = false;
   bool leadingZeros = false;
   bool disableDecPoint = false;
-  sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments,
-               updateWithDelays, leadingZeros, disableDecPoint);
+  sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins,
+               resistorsOnSegments, updateWithDelays, leadingZeros,
+               disableDecPoint);
 
   pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin_l, INPUT_PULLUP);
-  pinMode(buttonPin_c, INPUT_PULLUP);
-  pinMode(buttonPin_r, INPUT_PULLUP);
+  pinMode(leftButton.pin, INPUT_PULLUP);
+  pinMode(centerButton.pin, INPUT_PULLUP);
+  pinMode(rightButton.pin, INPUT_PULLUP);
 
   lastInteractionTime = millis();
 }
 
-// Function to check for button press with debouncing
-bool checkButton(int buttonPin, bool &buttonState, bool &lastButtonState, unsigned long &lastDebounceTime, unsigned long debounceDelay = 50) {
-  int reading = digitalRead(buttonPin);
-
-  // If the button state has changed (due to noise or press)
-  if (reading != lastButtonState) {
-    // reset the debouncing timer
-    lastDebounceTime = millis();
-  }
-
-  // Check if the debounce time has passed
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    // If the button state has changed and the debounce time has passed
-    if (reading != buttonState) {
-      buttonState = reading;
-
-      // Only return true if the button is pressed
-      if (buttonState == LOW) {
-        return true;
-      }
-    }
-  }
-
-  // Save the reading as the last state for the next loop
-  lastButtonState = reading;
-
-  // Return false if no button press is detected
-  return false;
-}
-
 int convertToPWM(float brightnessValue) {
-  // convert value between 0~1 to brightness value between 0~255 using an exponential function so that it feels natural to a human
   brightnessValue = constrain(brightnessValue, 0, 1);
-  if (brightnessValue == 0)
+  if (brightnessValue < 0.001)
     return 0;
   float e = 2.71828; // Euler's number
-  int pwmValue = pow(e, 5*brightnessValue)/pow(e,5) * 255;
+  int pwmValue = pow(e, 5 * brightnessValue) / pow(e, 5) * 255;
   return pwmValue;
 }
 
-void loop() {
-  unsigned long currentMillis = millis();
-  sevseg.refreshDisplay();
-
-  // read the button output
-  // note: buttonDown is true if the button was pressed down in this step, buttonPushed is whether it is currently pressed
-  bool buttonDown_l = checkButton(buttonPin_l, buttonState_l, lastButtonState_l, lastDebounceTime_l);
-  bool buttonPushed_l = (buttonState_l == LOW);
-  bool buttonReleased_l = (!buttonPushed_l && lastButtonPushed_l);
-
-  bool buttonDown_c = checkButton(buttonPin_c, buttonState_c, lastButtonState_c, lastDebounceTime_c);
-  bool buttonPushed_c = (buttonState_c == LOW);
-  bool buttonReleased_c = (!buttonPushed_c && lastButtonPushed_c);
-
-  bool buttonDown_r = checkButton(buttonPin_r, buttonState_r, lastButtonState_r, lastDebounceTime_r);
-  bool buttonPushed_r = (buttonState_r == LOW);
-  bool buttonReleased_r = (!buttonPushed_r && lastButtonPushed_r);
-
-  // Handle left button (decrement time)
-  if (buttonDown_l) {
+void handleTimeAdjustment(Button &button, bool isIncrement) {
+  if (button.checkButton()) {
     lastInteractionTime = currentMillis;
     if (!displayOn) {
       displayOn = true;
-      // ignore the press which turns on the display
-      ignoreCurrentPress_l = true;
+      button.ignoreCurrentPress = true;
     } else {
-      leftButtonPressStartTime = currentMillis;
-      ignoreCurrentPress_l = false;
+      button.pressStartTime = currentMillis;
+      button.ignoreCurrentPress = false;
     }
   }
 
-  if (displayOn && !ignoreCurrentPress_l && buttonPushed_l && timerPaused) {
-    unsigned long pressDuration = currentMillis - leftButtonPressStartTime;
-    unsigned long interval = BASE_INTERVAL - (BASE_INTERVAL - MIN_INTERVAL) * constrain((float)pressDuration / MAX_PRESS_DURATION, 0, 1);
+  if (displayOn && !button.ignoreCurrentPress && button.isPressed() &&
+      timerPaused) {
+    unsigned long pressDuration = currentMillis - button.pressStartTime;
+    unsigned long interval = BASE_INTERVAL -
+                             (BASE_INTERVAL - MIN_INTERVAL) *
+                                 constrain((float)pressDuration /
+                                               MAX_PRESS_DURATION,
+                                           0, 1);
     interval = constrain(interval, MIN_INTERVAL, BASE_INTERVAL);
 
-    if ((currentMillis - lastIncrementTime_l) >= interval) {
-      unsigned long decrementAmount = 1UL * 60 * 1000; // 1 minute
+    if ((currentMillis - button.lastIncrementTime) >= interval) {
+      unsigned long adjustAmount = 1UL * 60 * 1000; // 1 minute
 
-      if (remainingTime >= decrementAmount) {
-        remainingTime -= decrementAmount;
+      if (isIncrement) {
+        remainingTime += adjustAmount;
       } else {
-        remainingTime = 0;
+        remainingTime = remainingTime >= adjustAmount
+                            ? remainingTime - adjustAmount
+                            : 0;
       }
       setDuration = remainingTime; // Update setDuration
-      lastIncrementTime_l = currentMillis;
+      button.lastIncrementTime = currentMillis;
     }
     lastInteractionTime = currentMillis;
   }
 
-  if (buttonReleased_l) {
-    leftButtonPressStartTime = 0;
-    ignoreCurrentPress_l = false;
-  }
-  lastButtonPushed_l = buttonPushed_l;
-
-  // Handle right button (increment time)
-  if (buttonDown_r) {
-    lastInteractionTime = currentMillis;
-    if (!displayOn) {
-      displayOn = true;
-      ignoreCurrentPress_r = true;
-    } else {
-      rightButtonPressStartTime = currentMillis;
-      ignoreCurrentPress_r = false;
-    }
+  if (button.isReleased()) {
+    button.pressStartTime = 0;
+    button.ignoreCurrentPress = false;
   }
 
-  if (displayOn && !ignoreCurrentPress_r && buttonPushed_r && timerPaused) {
-    unsigned long pressDuration = currentMillis - rightButtonPressStartTime;
-    unsigned long interval = BASE_INTERVAL - (BASE_INTERVAL - MIN_INTERVAL) * constrain((float)pressDuration / MAX_PRESS_DURATION, 0, 1);
+  button.updateLastButtonPushed();
+}
 
-    if ((currentMillis - lastIncrementTime_r) >= interval) {
-      unsigned long incrementAmount = 1UL * 60 * 1000; // 1 minute
+void handleCenterButton(Button &button) {
+  static bool centerButtonLongPressHandled = false;
+  static unsigned long centerButtonPressTime = 0;
 
-      remainingTime += incrementAmount;
-      setDuration = remainingTime; // Update setDuration
-      lastIncrementTime_r = currentMillis;
-    }
-    lastInteractionTime = currentMillis;
-  }
-
-  if (buttonReleased_r) {
-    rightButtonPressStartTime = 0;
-    ignoreCurrentPress_r = false;
-  }
-  lastButtonPushed_r = buttonPushed_r;
-
-  // Handle center button (pause/restart and reset)
-  if (buttonDown_c) {
+  if (button.checkButton()) {
     centerButtonPressTime = currentMillis;
     centerButtonLongPressHandled = false;
     lastInteractionTime = currentMillis;
     if (!displayOn) {
       displayOn = true;
-      ignoreCurrentPress_c = true;
+      button.ignoreCurrentPress = true;
     } else {
-      ignoreCurrentPress_c = false;
+      button.ignoreCurrentPress = false;
     }
   }
 
-  if (displayOn && !ignoreCurrentPress_c && buttonPushed_c) {
+  if (displayOn && !button.ignoreCurrentPress && button.isPressed()) {
     if (!centerButtonLongPressHandled) {
       unsigned long pressDuration = currentMillis - centerButtonPressTime;
       if (pressDuration >= LONG_PRESS_DURATION) {
@@ -239,35 +196,37 @@ void loop() {
     }
   }
 
-  if (buttonReleased_c) {
+  if (button.isReleased()) {
     unsigned long pressDuration = currentMillis - centerButtonPressTime;
-    if (displayOn && !centerButtonLongPressHandled && pressDuration < LONG_PRESS_DURATION && !ignoreCurrentPress_c) {
+    if (displayOn && !centerButtonLongPressHandled &&
+        pressDuration < LONG_PRESS_DURATION && !button.ignoreCurrentPress) {
       // Short press detected
       timerPaused = !timerPaused; // Toggle pause/restart
       lastInteractionTime = currentMillis;
     }
     centerButtonPressTime = 0;
-    ignoreCurrentPress_c = false;
+    button.ignoreCurrentPress = false;
   }
 
-  lastButtonPushed_c = buttonPushed_c;
+  button.updateLastButtonPushed();
+}
 
-  // Update the remaining time when the timer is running
+void updateTimer() {
   if (!timerPaused && (currentMillis - lastTimerUpdateTime >= 1000)) {
-    if (remainingTime >= 1000) {
-      remainingTime -= 1000;
-    } else {
-      remainingTime = 0;
+    remainingTime = remainingTime >= 1000 ? remainingTime - 1000 : 0;
+
+    if (remainingTime == 0) {
       timerPaused = true; // Stop the timer when it reaches zero
       // Start LED auto-off timer
       ledTurnOffTime = currentMillis + LED_AUTO_OFF_DURATION;
     }
     lastTimerUpdateTime = currentMillis;
   }
+}
 
+void updateDisplay() {
   // Handle display blinking when paused
-  // don't blink when the time is being adjusted
-  if (timerPaused && !(buttonPushed_l || buttonPushed_r)) {
+  if (timerPaused && !(leftButton.isPressed() || rightButton.isPressed())) {
     if (currentMillis - lastBlinkTime >= 300) {
       displayBlinkState = !displayBlinkState;
       lastBlinkTime = currentMillis;
@@ -276,38 +235,6 @@ void loop() {
     displayBlinkState = true;
   }
 
-  // Adjust LED brightness
-  float targetBrightness = 0;
-  unsigned long rampup_dur = 20UL * 60 * 1000; // how long to ramp up to full brightness
-  if (remainingTime <= rampup_dur) {
-    targetBrightness = (rampup_dur - remainingTime) / (float)rampup_dur;
-  } else {
-    targetBrightness = 0;
-  }
-
-  if (timerPaused)
-    targetBrightness = constrain(targetBrightness, 0, 0.2); // Cap brightness to 20%
-
-  // Turn off LED after auto-off duration
-  if (ledTurnOffTime != 0 && currentMillis >= ledTurnOffTime) {
-    targetBrightness = 0;
-    ledTurnOffTime = 0;
-  }
-
-  // Exponential smoothing
-  currentBrightness = smoothingFactor * targetBrightness + (1.0 - smoothingFactor) * currentBrightness;
-
-  int pwmValue = convertToPWM(constrain(currentBrightness, 0, 1) * 0.8); // apply 80% limit for safety
-  analogWrite(ledPin, pwmValue);
-
-  // Turn off the display after 10 seconds of no interaction
-  if (currentMillis - lastInteractionTime >= 10000) {
-    displayOn = false;
-  } else {
-    displayOn = true;
-  }
-
-  // Display the remaining time
   if (displayOn) {
     if (displayBlinkState) {
       unsigned long totalMinutes = remainingTime / 60000UL;
@@ -321,4 +248,53 @@ void loop() {
   } else {
     sevseg.blank();
   }
+}
+
+void updateBrightness() {
+  // Adjust LED brightness
+  float targetBrightness = 0;
+  unsigned long rampup_dur = 20UL * 60 * 1000; // Ramp up over 20 minutes
+  if (remainingTime <= rampup_dur) {
+    targetBrightness = (rampup_dur - remainingTime) / (float)rampup_dur;
+  } else {
+    targetBrightness = 0;
+  }
+
+  if (timerPaused)
+    targetBrightness = constrain(targetBrightness, 0, 0.2); // Cap at 20%
+
+  // Turn off LED after auto-off duration
+  if (ledTurnOffTime != 0 && currentMillis >= ledTurnOffTime) {
+    targetBrightness = 0;
+    ledTurnOffTime = 0;
+  }
+
+  // Exponential smoothing
+  currentBrightness = smoothingFactor * targetBrightness +
+                      (1.0 - smoothingFactor) * currentBrightness;
+
+  int pwmValue =
+      convertToPWM(constrain(currentBrightness, 0, 1) * 0.8); // Max 80%
+  analogWrite(ledPin, pwmValue);
+}
+
+void checkInactivity() {
+  // Turn off the display after 10 seconds of no interaction
+  displayOn = (currentMillis - lastInteractionTime < 10000);
+}
+
+void loop() {
+  currentMillis = millis();
+  sevseg.refreshDisplay();
+
+  // Handle buttons
+  handleTimeAdjustment(leftButton, false); // Decrement time
+  handleTimeAdjustment(rightButton, true); // Increment time
+  handleCenterButton(centerButton);
+
+  // Update timer and display
+  updateTimer();
+  updateDisplay();
+  updateBrightness();
+  checkInactivity();
 }
